066da9810ea469b4a2b312fcec999e0800dab50a
Ibrahim Altamimi <ibrahim7ma@gmail.com>
2021-12-10 21:12:42 +0300

    Bug Fix

--- Lab05/src/GameStore1.java
+++ Lab05/src/GameStore1.java
@@ -6,22 +6,23 @@ public class GameStore1 {
 		System.out.println("Welcome to Gaming Center :).");
 		System.out.print("Please, enter game id: ");
 		int GameId = input.nextInt();
-		System.out.print("Please, enter thge price of a game: ");
+		System.out.print("Please, enter the price of a game: ");
 		double price = input.nextDouble();
 		System.out.print("Please, enter the number of games: ");
 		double NumberOfGames = input.nextDouble();
 		
+		double total = 0;
 		if(NumberOfGames < 1)
 			System.out.println("Error");
 		else {
 			
-			double total = price*NumberOfGames;
+			total = price*NumberOfGames;
 			if(NumberOfGames > 2) {
 			double discount = (20.0/100);
 		     total -= discount*total; }
 			
 		}
-		System.out.print("Total price for game "+GameId+" is: "+ total +"SR");
+		System.out.print("Total price for game " + GameId + " is: " + total + "SR");
 
 	}
 
