2bac832efc0d77d9d34a1af2274d3371c5c0b96e
Ibrahim Altamimi <ibrahim7ma@gmail.com>
2021-12-11 15:25:45 +0300

    Fixes

---
+++ Assignment01-02/src/BMI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BMI {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter weight in pounds: ");
+		double weight = input.nextDouble();
+		System.out.print("Enter height in inches: ");
+		double height = input.nextDouble();
+		
+		double bmi = (weight*0.45359237) / Math.pow((height*0.0254),2);
+		System.out.print("BMI is "+bmi);
+
+	}
+
+}
